0e9112dd0fdb3b2adf3c3a68ee3ec193315f84f0
hik8hik <hik8hik@gmail.com>
2022-03-12 08:50:19 +0000

    DB Connection

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true,
+  });
+
+  console.log("MONGO DB CONNECTED");
+};
+
+module.exports = connectDB;
