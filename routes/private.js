9d8dc413d52c2bc69e38c309a5c53df53ff64a60
hik8hik <hik8hik@gmail.com>
2022-03-12 20:23:34 +0000

    Protected access to private data

---
+++ routes/private.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const {getPrivateData} = require("../controllers/private") 
+
+router.route("/").get(getPrivateData);
+
+module.exports = router;
