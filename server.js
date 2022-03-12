6702db8235000d737e2d08a4bee5d05950b8288d
hik8hik <hik8hik@gmail.com>
2022-03-12 05:46:33 +0000

    Redirecting middleware

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-require("dotenv").config({path: "./config.env"})
+require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 
 const app = express();
 
 //middleware
-app.use(express.json())
+app.use(express.json());
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
