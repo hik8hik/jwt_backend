316d469d3ddc5940e07e4e4d107700e5e4d25453
hik8hik <hik8hik@gmail.com>
2022-03-12 05:43:26 +0000

    Middleware to get request from body

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 
 const app = express();
 
+//middleware
+app.use(express.json())
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
