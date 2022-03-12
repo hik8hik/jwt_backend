5dfdd1c43ca16f8976a5697d7951dd6d4606bb50
hik8hik <hik8hik@gmail.com>
2022-03-12 20:09:49 +0000

    protection provider middleware. deciphering of token

---
+++ middleware/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jwt");
+const User = require("../models/User");
+const ErrorResponse = require("../utils/errorResponse");
+
+exports.protect = async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    //sample headers is (Bearer sdjsffbsfb_jsonwebtoken_cvcnv) so we split on space ""
+    //and pick the second split [1].
+    token = req.headers.authorization.split(" ")[1];
+  }
+
+  if (!token) {
+    return next(new ErrorResponse("Not Authorised to access this route", 401));
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRETf);
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return next(new ErrorResponse("No user was found with this id", 404));
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return next(new ErrorResponse("Not authorized to access this route", 401));
+  }
+};
