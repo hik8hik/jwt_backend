43c4b0122168cfe85fcd385a5b7ded3f48e2c252
hik8hik <hik8hik@gmail.com>
2022-03-12 09:43:17 +0000

    Created Crash Error Handler

--- server.js
+++ server.js
@@ -13,4 +13,11 @@ app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+process.on(unhandlesRejection, (err, promise) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1))
+});
