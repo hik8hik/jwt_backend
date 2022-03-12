548aef0344bc0534a923e72628d36392cba2ac5c
hik8hik <hik8hik@gmail.com>
2022-03-12 08:37:43 +0000

    Middleware to do password hashing

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = reqiuire("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -22,6 +23,17 @@ const UserSchema = new mongoose.Schema({
   },
   resetPaswwordToken: String,
   resetpasswordExpire: Date,
+});
+
+//mongoose middlewares
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model("User", UserSchema);
