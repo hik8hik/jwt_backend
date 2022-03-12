0c6213c82943fc3ab7f2c3e70d12b2d5a6297643
hik8hik <hik8hik@gmail.com>
2022-03-12 15:07:23 +0000

    JSONWEBTOKEN intergration

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,10 +12,7 @@ exports.register = async (req, res, next) => {
       password,
     });
 
-    res.status(201).json({
-      success: true,
-      user,
-    });
+    sendToken(user, 201, res);
   } catch (error) {
     next(error);
   }
@@ -39,7 +36,7 @@ exports.login = async (req, res, next) => {
       return next(new ErrorResponse("Invalid Credentials", 401));
     }
 
-    res.status(200).json({ success: true, token: "sdjdsjdf343dff" });
+    sendToken(user, 200, res);
   } catch (error) {
     res.status(500).json({ success: false, error: error.message });
   }
@@ -51,4 +48,9 @@ exports.forgotpassword = (req, res, next) => {
 
 exports.resetpassword = (req, res, next) => {
   res.send("Reset Password Route");
+};
+
+const sendToken = (user, statusCode, res) => {
+  const token = user.getSignedToken();
+  res.status(statusCode).json({ success: true, token });
 };
